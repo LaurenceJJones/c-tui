6e3202f67b7a3bad5bf31a8ae18ecf23a5a9537b
Laurence <laurence.jones@live.co.uk>
2024-02-06 23:51:56 +0000

    wip: day 2 progress, created key based authentication with a custom handler

--- config.go
+++ config.go
@@ -1,39 +1,50 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
+	"path"
 
 	"github.com/charmbracelet/log"
 	yaml "gopkg.in/yaml.v2"
 )
 
 type Config struct {
-	Addr string
+	Addr           string   `yaml:"addr"`
+	TrustedKeys    []string `yaml:"trusted_keys"`
+	LogLevel       string   `yaml:"log_level"`
+	authorizedKeys string   `yaml:"-"` //Private field for user process keys
 }
 
-func NewConfig(path string) (*Config, error) {
-	//read path and parse yaml
+func NewConfig() (*Config, error) {
+	path := flag.String("config", "", "path to the config file")
+	debug := flag.Bool("debug", false, "debug log level")
+	flag.Parse()
+	if *debug {
+		log.SetLevel(log.DebugLevel)
+	}
 	config := &Config{}
-	if err := loadConfigFromPath(config, path); err != nil {
+	if err := loadConfigFromPath(config, *path); err != nil {
 		return nil, err
 	}
-	setDefaults(config)
-	return config, nil
+	return config, setDefaults(config) //return the config and error from the default settings
 }
 
+// Load the configuration from the file
 func loadConfigFromPath(config *Config, path string) error {
 	if path != "" {
-		log.Info("loading configuration from file", "path", path)
 		yamlFile, err := os.Open(path)
 		if err == nil {
+			log.Info("loading configuration from file", "path", path)
 			defer yamlFile.Close()
-			//process the yaml
 			dec := yaml.NewDecoder(yamlFile)
 			dec.SetStrict(true)
 			if err := dec.Decode(config); err != nil {
 				return fmt.Errorf("can't parse configuration file %s : %s", path, err)
 			}
+		} else if !os.IsNotExist(err) {
+			return fmt.Errorf("can't open configuration file %s : %s", path, err)
 		}
 	}
 	return nil
@@ -42,9 +53,25 @@ func loadConfigFromPath(config *Config, path string) error {
 /*
 Set default values for the configuration
 */
-func setDefaults(config *Config) {
+func setDefaults(config *Config) error {
 	//Set default addr to all listeners port 1337
 	if config.Addr == "" {
 		config.Addr = "0.0.0.0:1337"
 	}
+	loadCurrentUserAuthorizedKeys(config)
+	return nil
+}
+
+func loadCurrentUserAuthorizedKeys(config *Config) {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		log.Debug("user home directory is not configured", "error", err)
+		return
+	}
+	authorizedKeysPath := path.Join(home, ".ssh", "authorized_keys")
+	if _, err := os.Stat(authorizedKeysPath); err != nil {
+		log.Debug("user has no authorized keys file", "error", err)
+		return
+	}
+	config.authorizedKeys = authorizedKeysPath
 }
--- main.go
+++ main.go
@@ -6,7 +6,6 @@ package main
 import (
 	"context"
 	"errors"
-	"flag"
 	"os"
 	"os/signal"
 	"syscall"
@@ -14,25 +13,14 @@ import (
 
 	"github.com/charmbracelet/log"
 	"github.com/charmbracelet/ssh"
-	"github.com/charmbracelet/wish"
-	lm "github.com/charmbracelet/wish/logging"
 )
 
 func main() {
-	configPath := flag.String("config", "", "path to the config file")
-	flag.Parse()
-	config, err := NewConfig(*configPath)
+	config, err := NewConfig()
 	if err != nil {
 		log.Error("could not load config", "error", err)
 	}
-	s, err := wish.NewServer(
-		wish.WithAddress(config.Addr),
-		wish.WithHostKeyPath(".ssh/term_info_ed25519"),
-		wish.WithMiddleware(
-			AppMiddleware(),
-			lm.Middleware(),
-		),
-	)
+	s, err := NewServer(config)
 	if err != nil {
 		log.Error("could not start server", "error", err)
 	}
