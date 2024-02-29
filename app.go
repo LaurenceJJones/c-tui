88ad1ea0d802bf75bc2b1f08c4ad0ddb00331eb4
Laurence <laurence.jones@live.co.uk>
2024-02-29 20:56:07 +0000

    wip: add example table and some styles

--- app.go
+++ app.go
@@ -1,21 +1,26 @@
 package main
 
 import (
-	"fmt"
 	"time"
 
+	"github.com/charmbracelet/bubbles/table"
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss/table"
+	"github.com/charmbracelet/lipgloss"
 	"github.com/charmbracelet/ssh"
 	"github.com/charmbracelet/wish"
 	bm "github.com/charmbracelet/wish/bubbletea"
 )
 
+var baseStyle = lipgloss.NewStyle().
+	BorderStyle(lipgloss.NormalBorder()).
+	BorderForeground(lipgloss.Color("240"))
+
 type TickMsg time.Time
 
 type session struct {
 	tea.Model
-	_term term
+	_term         term
+	DecisionTable table.Model
 }
 
 type term struct {
@@ -35,18 +40,26 @@ func (m session) Init() tea.Cmd {
 }
 
 func (m session) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
 	switch msg := msg.(type) {
 	case tea.WindowSizeMsg:
 		m._term.updateHW(msg.Height, msg.Width)
 	case tea.KeyMsg:
 		switch msg.String() {
+		case "esc":
+			if m.DecisionTable.Focused() {
+				m.DecisionTable.Blur()
+			} else {
+				m.DecisionTable.Focus()
+			}
 		case "q", "ctrl+c":
 			return m, tea.Quit
 		}
 	case TickMsg:
 		return m, fetchData()
 	}
-	return m, nil
+	m.DecisionTable, cmd = m.DecisionTable.Update(msg)
+	return m, cmd
 }
 
 func AppMiddleware() wish.Middleware {
@@ -63,6 +76,7 @@ func teaHandler(s ssh.Session) (tea.Model, []tea.ProgramOption) {
 		wish.Fatalln(s, "no active terminal, skipping")
 		return nil, nil
 	}
+	columnWidth := pty.Window.Width / 2 / 4
 	m := session{
 		_term: term{
 			user:   s.Context().User(),
@@ -70,6 +84,24 @@ func teaHandler(s ssh.Session) (tea.Model, []tea.ProgramOption) {
 			width:  pty.Window.Width,
 			height: pty.Window.Height,
 		},
+		DecisionTable: table.New(
+			table.WithColumns([]table.Column{
+				{Title: "id", Width: columnWidth},
+				{Title: "type", Width: columnWidth},
+				{Title: "value", Width: columnWidth},
+				{Title: "duration", Width: columnWidth},
+			}),
+			table.WithRows([]table.Row{
+				{"1", "type1", "value1", "duration1"},
+				{"1", "type1", "value1", "duration1"},
+				{"1", "type1", "value1", "duration1"},
+				{"1", "type1", "value1", "duration1"},
+				{"1", "type1", "value1", "duration1"},
+				{"1", "type1", "value1", "duration1"},
+			}),
+			table.WithFocused(true),
+			table.WithHeight(pty.Window.Height/2),
+		),
 	}
 	return m, []tea.ProgramOption{tea.WithAltScreen()}
 }
@@ -82,9 +114,18 @@ func fetchData() tea.Cmd {
 }
 
 func (m session) View() string {
-	s := table.New().Width(80).Headers([]string{"TERM", "WIDTH", "HEIGHT", "USER"}...).Rows([][]string{
-		{m._term.term, fmt.Sprintf("%d", m._term.width), fmt.Sprintf("%d", m._term.height), m._term.user},
-	}...)
-
-	return s.Render()
+	s := table.DefaultStyles()
+	s.Header = s.Header.
+		BorderStyle(lipgloss.RoundedBorder()).
+		BorderForeground(lipgloss.Color("240")).
+		BorderBottom(true).
+		Bold(false)
+	if m.DecisionTable.Focused() {
+		s.Selected = s.Selected.
+			Foreground(lipgloss.Color("229")).
+			Background(lipgloss.Color("57")).
+			Bold(true)
+	}
+	m.DecisionTable.SetStyles(s)
+	return baseStyle.Render(m.DecisionTable.View()) + "\n"
 }
--- go.mod
+++ go.mod
@@ -14,6 +14,7 @@ require (
 require (
 	github.com/anmitsu/go-shlex v0.0.0-20200514113438-38f4b401e2be // indirect
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/charmbracelet/bubbles v0.18.0
 	github.com/charmbracelet/keygen v0.5.0 // indirect
 	github.com/charmbracelet/x/errors v0.0.0-20240202113029-6ff29cf0473e // indirect
 	github.com/charmbracelet/x/exp/term v0.0.0-20240202113029-6ff29cf0473e // indirect
